e2d9d292a32fcf589899b11978a58216bbf4a6b8
Albert-Brusca <abrusca@iesebre.com>
2021-11-23 16:45:24 +0100

    Deloyment a Github Pages

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,16 @@ set -e
 
 npm run build
 
+cd dist
 
 git init
 git add -A
 git commit -m 'deploy'
 
-git remote add origin https://github.com/Albert-Brusca/ComptadorIonic.git || true
+git remote add origin https://github.com/Albert-Brusca/comptador-ionic.git || true
 
 git branch -M main || true
 
-git push -f https://github.com/Albert-Brusca/ComptadorIonic.git main
+git push -f https://github.com/Albert-Brusca/comptador-ionic.git main
 
 cd -
